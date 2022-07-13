25b3a066a15898150b2705bfb941425db79d37df
Alexander Lind <alex.anonym@gmail.com>
2022-07-13 13:15:25 +0200

    Finished FizzBuzz exercise

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,21 @@
 // TODO: Write your function in this file
 
+function fizzBuzz(lower, upper) {
+  const arr = []
+  for (i = lower; i <= upper; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push('FizzBuzz')
+    } else if (i % 3 === 0) {
+      arr.push('Fizz')
+    } else if (i % 5 === 0) {
+      arr.push('Buzz')
+    } else {
+      arr.push(i)
+    }
+  }
+  return arr
+}
+
+console.log(fizzBuzz(1, 15))
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzBuzz
